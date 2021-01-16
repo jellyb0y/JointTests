6d1fbae1c3435487c5a0b079282003ab04f6ceff
jellyB0y <kisel@internet.ru>
2021-01-17 02:05:32 +0300

    some fix

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -3,7 +3,7 @@ import path from 'path';
 export const WSS_PORT = 4433;
 export const SESSION_TIMEOUT = 5000;
 export const MAX_SESSIONS = 30;
-export const CALLBACK_BOUNCE_TIME = 1000;
+export const CALLBACK_DEBOUNCE_TIME = 1000;
 
 export const SERVER_SECRET = 'topSecret';
 export const SSL_KEY_PATH = path.resolve(__dirname, '../key.pem');
